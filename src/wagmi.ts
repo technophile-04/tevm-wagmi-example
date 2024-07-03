89b0f28d2ddbf30ccde8014e8de9d4cfe703ec1a
Shiv Bhonde <shivbhonde04@gmail.com>
2024-07-04 08:15:03 +0900

    format wagmi

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -6,30 +6,30 @@ import { createMemoryClient } from "tevm";
 import { createCommon, tevmDefault } from "tevm/common";
 
 const customCommon = createCommon({
-...tevmDefault,
-id: hardhat.id,
-loggingLevel: "warn",
-eips: [],
-hardfork: "cancun",
+  ...tevmDefault,
+  id: hardhat.id,
+  loggingLevel: "warn",
+  eips: [],
+  hardfork: "cancun",
 });
 export const memoryClient = createMemoryClient({ common: customCommon });
 
 export const config = createConfig({
-chains: [hardhat],
-connectors: [
-injected(),
-coinbaseWallet({ appName: "Create Wagmi" }),
-walletConnect({ projectId: "3a8170812b534d0ff9d794f19a901d64" }),
-],
-ssr: true,
-transports: {
-[hardhat.id]: custom(memoryClient),
-},
-cacheTime: 0,
+  chains: [hardhat],
+  connectors: [
+    injected(),
+    coinbaseWallet({ appName: "Create Wagmi" }),
+    walletConnect({ projectId: "3a8170812b534d0ff9d794f19a901d64" }),
+  ],
+  ssr: true,
+  transports: {
+    [hardhat.id]: custom(memoryClient),
+  },
+  cacheTime: 0,
 });
 
 declare module "wagmi" {
-interface Register {
-config: typeof config;
-}
+  interface Register {
+    config: typeof config;
+  }
 }
